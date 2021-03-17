c30fc22704f7d8e790fa126532b2dc547c2413e5
Sabrineh <Sabrinehmokhtari@yahoo.com>
2021-03-17 18:26:45 +0330

    added tests for computeArea

--- src/test/RecTest.java
+++ src/test/RecTest.java
@@ -6,15 +6,39 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class RecTest {
     @Test
-    public void test1() {
+    public void PositiveFloatWidthIntHeight() throws Exception {
         assertEquals(6, new MyRect(2, 3).ComputeArea());
     }
     @Test
-    public void test2() {
+    public void PositiveFloatWidthFloatHeight() throws Exception {
         assertEquals(5.4, new MyRect(2.0, 2.7).ComputeArea());
     }
     @Test
-    public void test3() {
-        assertEquals(10, new MyRect(5, 2).ComputeArea());
+    public void PositiveIntWidthFloatHeight() throws Exception {
+        assertEquals(10.5, new MyRect(5, 2.1).ComputeArea());
+    }
+
+    @Test
+    public void throwsExceptionWhenZeroNumbersAreGiven() throws Exception {
+        Exception exception = assertThrows(ArithmeticException.class, () -> {
+            new MyRect(0, 2).ComputeArea();
+        });
+
+        String expectedMessage = "width or height are not positive";
+        String actualMessage = exception.getMessage();
+
+        assertTrue(actualMessage.contains(expectedMessage));
+    }
+
+    @Test
+    public void throwsExceptionWhenNonNegativeNumbersAreGiven() throws Exception {
+        Exception exception = assertThrows(ArithmeticException.class, () -> {
+            new MyRect(2, -1).ComputeArea();
+        });
+
+        String expectedMessage = "width or height are not positive";
+        String actualMessage = exception.getMessage();
+
+        assertTrue(actualMessage.contains(expectedMessage));
     }
 }
