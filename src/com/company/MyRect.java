fe8ca0ec0eba8ea4b0630ec84b53bd118ab9d1b0
Sabrineh <Sabrinehmokhtari@yahoo.com>
2021-03-17 19:07:42 +0330

    Write Setter and Getter signatures

--- src/com/company/MyRect.java
+++ src/com/company/MyRect.java
@@ -6,13 +6,31 @@ public class MyRect {
         this.height = height;
     }
 
+    public double getWidth() {
+        return -1;
+    }
+
+    public void setWidth(double width) {
+    }
+
+    public double getHeight() {
+        return -1;
+    }
+
+    public void setHeight(double height) {
+    }
+
     private double width;
     private double height;
 
     public double ComputeArea() throws Exception{
+        // PreCondition
         if (width <= 0 || height <= 0){
             throw new ArithmeticException("width or height are not positive");
         }
+        //PostCondition
         return (width * height);
     }
 }
+
+
