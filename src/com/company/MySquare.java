cd1159b513e48430788ce472f83310642201b564
hassan <h.zaker2222@gmail.com>
2021-03-17 19:24:44 +0330

    MySquare implemented as a subclass of MyRect (signatures only)

---
+++ src/com/company/MySquare.java
@@ -0,0 +1,33 @@
+package com.company;
+
+public class MySquare extends MyRect {
+
+    public MySquare(double width) {
+        super(width, width);
+    }
+
+    @Override
+    public double getWidth() {
+        return super.getWidth();
+    }
+
+    @Override
+    public void setWidth(double width) {
+        super.setWidth(width);
+    }
+
+    @Override
+    public double getHeight() {
+        return super.getHeight();
+    }
+
+    @Override
+    public void setHeight(double height) {
+        super.setHeight(height);
+    }
+
+    @Override
+    public double ComputeArea() throws Exception {
+        return super.ComputeArea();
+    }
+}
