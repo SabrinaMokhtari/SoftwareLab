fd2c09d196f1c16314cb4ff652dba3bc0fb8e4ae
Sabrineh <Sabrinehmokhtari@yahoo.com>
2021-04-09 16:39:58 +0430

    Power -> added RecursiveMultiplication body

--- src/com/company/EXP3/Bridge/RecursiveMultiplication.java
+++ src/com/company/EXP3/Bridge/RecursiveMultiplication.java
@@ -1,8 +1,14 @@
 package com.company.EXP3.Bridge;
 
-public class RecursiveMultiplication implements Multiplication{
+public class RecursiveMultiplication implements Multiplication {
     @Override
     public int mul(int a, int b) {
-        return 0;
+        if (a == 0 || b == 0) {
+            return 0;
+        } else if (b < 0) {
+            return -a + mul(a, b + 1);
+        } else {
+            return a + mul(a, b - 1);
+        }
     }
 }
