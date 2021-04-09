d57de615f0d4bc59957f92ac6bc73e2650ac53db
hassan <h.zaker2222@gmail.com>
2021-04-09 13:05:05 +0430

    Power -> Test file create

---
+++ src/test/Exp3/Bridge/PowerTests.java
@@ -0,0 +1,10 @@
+package test.Exp3.Bridge;
+
+import com.company.EXP3.Bridge.*;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class PowerTests {
+
+}
