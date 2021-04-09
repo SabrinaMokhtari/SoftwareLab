aa9cc20e738df647fe9b04961a6439fd0864e35a
Sabrineh <Sabrinehmokhtari@yahoo.com>
2021-04-09 16:33:17 +0430

    Power -> added AddMultiplication body

--- src/com/company/EXP3/Bridge/AddMultiplication.java
+++ src/com/company/EXP3/Bridge/AddMultiplication.java
@@ -3,6 +3,10 @@ package com.company.EXP3.Bridge;
 public class AddMultiplication implements Multiplication {
     @Override
     public int mul(int a, int b) {
-        return 0;
+        int ans = 0;
+        for (int i=0 ; i < b ; i++){
+            ans += a;
+        }
+        return ans;
     }
 }
